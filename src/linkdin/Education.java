240a4f7a70208db4fb55b3d4b8592da1f7df8932
AjavSingh <ajavsinghraghavkrj20@gmail.com>
2025-03-29 16:44:42 +0530

    added eduction attributes

--- src/linkdin/Education.java
+++ src/linkdin/Education.java
@@ -1,4 +1,12 @@
 package linkdin;
 
+import javax.lang.model.element.NestingKind;
+
 public class Education {
+    private String school;
+    private String degree;
+    private String filedOfStudy;
+    private String starDate;
+    private String endDate;
+
 }
