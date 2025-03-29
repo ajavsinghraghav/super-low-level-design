3414eb4fda8dc227e9786cbc73d3d26cd6b34296
AjavSingh <ajavsinghraghavkrj20@gmail.com>
2025-03-29 16:48:58 +0530

    experince attributes added

--- src/linkdin/Experince.java
+++ src/linkdin/Experince.java
@@ -1,4 +1,12 @@
 package linkdin;
 
 public class Experince {
+    private String title;
+    private String comapany;
+    private String startDate;
+    private String endDate;
+    private String description;
+
+
+
 }
