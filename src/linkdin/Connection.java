6acf1b04f43b2fe2b99298cf4dd2a54c6bc51754
AjavSingh <ajavsinghraghavkrj20@gmail.com>
2025-03-29 17:11:29 +0530

    connection class succesfully added

--- src/linkdin/Connection.java
+++ src/linkdin/Connection.java
@@ -1,5 +1,20 @@
 package linkdin;
 
 public class Connection {
+    private User user;
+    private TimeStamp connectionDate;
 
+    public Connection(User user, TimeStamp connectionDate) {
+        this.user = user;
+        this.connectionDate = connectionDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public TimeStamp getConnectionDate() {
+        return connectionDate;
+    }
 }
+
