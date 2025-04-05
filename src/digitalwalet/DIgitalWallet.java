8668382fd36ae2738d678aefa0f921d91eac37fe
AjavSingh <ajavsinghraghavkrj20@gmail.com>
2025-04-05 07:02:36 +0530

    functional requirnments

---
+++ src/digitalwalet/DIgitalWallet.java
@@ -0,0 +1,9 @@
+package digitalwalet;
+
+public class DIgitalWallet extends User {
+    @Override
+    public void printUser() {
+        super.printUser();
+    }
+
+}
---
+++ src/digitalwalet/User.java
@@ -0,0 +1,7 @@
+package digitalwalet;
+
+public class User {
+    public void printUser(){
+        System.out.println(" this is my use class");
+    }
+}
